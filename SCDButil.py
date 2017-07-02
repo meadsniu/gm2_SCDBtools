49d97c23a71dd03cfd5077c337fa7f03853ef64b
meadsniu.dahlquist <meadsniu@gmail.com>
2017-07-02 06:50:27 -0500

    Updates and added functionality. Working bare bones version of make_all_temp_plots.py

--- SCDButil.py
+++ SCDButil.py
@@ -14,9 +14,82 @@ import ROOT
 
 class SCDButil:
     """
-    SCDButil class is a utility class for accessign information in the 
+    SCDButil class is a utility class for accessing information in the 
     slow controls database
     """
+
+    # dictionary to hold the marker colors for various subchannels
+    color_dict = { ('mscb323_Temp_P1', 0):ROOT.kRed, 
+                   ('mscb323_Temp_P1', 1):ROOT.kRed+1,
+                   ('mscb323_Temp_P1', 2):ROOT.kRed+2,
+                   ('mscb13e_Temp_P1', 3):ROOT.kRed+3,
+                   ('mscb13e_Temp_P1', 0):ROOT.kOrange, 
+                   ('mscb13e_Temp_P1', 1):ROOT.kOrange+1,
+                   ('mscb13e_Temp_P1', 2):ROOT.kOrange+2,
+                   ('mscb13e_Temp_P1', 3):ROOT.kOrange+3,
+                   ('mscb323_Temp_P1', 4):ROOT.kYellow, 
+                   ('mscb323_Temp_P1', 5):ROOT.kYellow+1,
+                   ('mscb323_Temp_P1', 6):ROOT.kYellow+2,
+                   ('mscb13e_Temp_P1', 7):ROOT.kYellow+3,
+                   ('mscb13e_Temp_P1', 4):ROOT.kGreen, 
+                   ('mscb13e_Temp_P1', 5):ROOT.kGreen+1,
+                   ('mscb13e_Temp_P1', 6):ROOT.kGreen+2,
+                   ('mscb13e_Temp_P1', 7):ROOT.kGreen+3,
+                   ('mscb323_Temp_P2', 0):ROOT.kTeal, 
+                   ('mscb323_Temp_P2', 1):ROOT.kTeal+1,
+                   ('mscb323_Temp_P2', 2):ROOT.kTeal+2,
+                   ('mscb13e_Temp_P2', 3):ROOT.kTeal+3,
+                   ('mscb13e_Temp_P2', 0):ROOT.kCyan, 
+                   ('mscb13e_Temp_P2', 1):ROOT.kCyan+1,
+                   ('mscb13e_Temp_P2', 2):ROOT.kCyan+2,
+                   ('mscb13e_Temp_P2', 3):ROOT.kCyan+3,
+                   ('mscb323_Temp_P2', 4):ROOT.kAzure, 
+                   ('mscb323_Temp_P2', 5):ROOT.kAzure+1,
+                   ('mscb323_Temp_P2', 6):ROOT.kAzure+2,
+                   ('mscb13e_Temp_P2', 7):ROOT.kAzure+3,
+                   ('mscb13e_Temp_P2', 4):ROOT.kBlue, 
+                   ('mscb13e_Temp_P2', 5):ROOT.kBlue+1,
+                   ('mscb13e_Temp_P2', 6):ROOT.kBlue+2,
+                   ('mscb13e_Temp_P2', 7):ROOT.kBlue+3,
+                   ('mscb323_Temp_P3', 0):ROOT.kViolet, 
+                   ('mscb323_Temp_P3', 1):ROOT.kViolet+1,
+                   ('mscb323_Temp_P3', 2):ROOT.kViolet+2,
+                   ('mscb13e_Temp_P3', 3):ROOT.kViolet+3,
+                   ('mscb13e_Temp_P3', 0):ROOT.kMagenta, 
+                   ('mscb13e_Temp_P3', 1):ROOT.kMagenta+1,
+                   ('mscb13e_Temp_P3', 2):ROOT.kMagenta+2,
+                   ('mscb13e_Temp_P3', 3):ROOT.kMagenta+3,
+                   ('mscb174_Temp_P1', 4):ROOT.kSpring-4,
+                   ('mscb174_Temp_P1', 5):ROOT.kSpring-3,
+                   ('mscb174_Temp_P1', 6):ROOT.kSpring-2,
+                   ('mscb174_Temp_P1', 7):ROOT.kSpring-1,
+                   ('mscb174_Temp_P5', 0):ROOT.kSpring,
+                   ('mscb174_Temp_P5', 1):ROOT.kSpring+1,
+                   ('mscb174_Temp_P5', 2):ROOT.kSpring+2,
+                   ('mscb174_Temp_P5', 3):ROOT.kSpring+3,
+                   ('mscb174_Temp_P5', 4):ROOT.kSpring+4,
+                   ('mscb174_Temp_P5', 5):ROOT.kSpring+5,
+                   ('mscb174_Temp_P5', 6):ROOT.kSpring+6,
+                   ('mscb174_Temp_P5', 7):ROOT.kSpring+7,
+                   ('mscb174_Temp_P7', 0):ROOT.kPink,
+                   ('mscb174_Temp_P7', 1):ROOT.kPink+1
+                 }
+
+    # dictionary to hold the list of subchannels for specific plots
+    subchannel_dict = {}
+    subchannel_dict['hall'] = [ ('mscb174_Temp_P1', 4), ('mscb174_Temp_P1', 5), ('mscb174_Temp_P1', 6), ('mscb174_Temp_P1', 7), ('mscb174_Temp_P5', 0), ('mscb174_Temp_P5', 1), ('mscb174_Temp_P5', 2), ('mscb174_Temp_P5', 3), ('mscb174_Temp_P5', 4), ('mscb174_Temp_P5', 5), ('mscb174_Temp_P5', 6), ('mscb174_Temp_P5', 7)]
+    subchannel_dict['laser_hut'] = [ ('mscb174_Temp_P7', 0), ('mscb174_Temp_P7', 1) ]
+    subchannel_dict['magnetA'] = [ ('mscb323_Temp_P1', 0), ('mscb323_Temp_P1', 1), ('mscb323_Temp_P1', 2), ('mscb323_Temp_P1', 3) ]
+    subchannel_dict['magnetB'] = [ ('mscb13e_Temp_P1', 0), ('mscb13e_Temp_P1', 1), ('mscb13e_Temp_P1', 2), ('mscb13e_Temp_P1', 3) ]
+    subchannel_dict['magnetC'] = [ ('mscb323_Temp_P1', 4), ('mscb323_Temp_P1', 5), ('mscb323_Temp_P1', 6), ('mscb323_Temp_P1', 7) ]
+    subchannel_dict['magnetD'] = [ ('mscb13e_Temp_P1', 4), ('mscb13e_Temp_P1', 5), ('mscb13e_Temp_P1', 6), ('mscb13e_Temp_P1', 7) ]
+    subchannel_dict['magnetE'] = [ ('mscb323_Temp_P2', 0), ('mscb323_Temp_P2', 1), ('mscb323_Temp_P2', 2), ('mscb323_Temp_P2', 3) ]
+    subchannel_dict['magnetF'] = [ ('mscb13e_Temp_P2', 0), ('mscb13e_Temp_P2', 1), ('mscb13e_Temp_P2', 2), ('mscb13e_Temp_P2', 3) ]
+    subchannel_dict['magnetG'] = [ ('mscb323_Temp_P2', 4), ('mscb323_Temp_P2', 5), ('mscb323_Temp_P2', 6), ('mscb323_Temp_P2', 7) ]
+    subchannel_dict['magnetH'] = [ ('mscb13e_Temp_P2', 4), ('mscb13e_Temp_P2', 5), ('mscb13e_Temp_P2', 6), ('mscb13e_Temp_P2', 7) ]
+    subchannel_dict['magnetI'] = [ ('mscb323_Temp_P3', 0), ('mscb323_Temp_P3', 1), ('mscb323_Temp_P3', 2), ('mscb323_Temp_P3', 3) ]
+    subchannel_dict['magnetJ'] = [ ('mscb13e_Temp_P3', 0), ('mscb13e_Temp_P3', 1), ('mscb13e_Temp_P3', 2), ('mscb13e_Temp_P3', 3) ]
+
     def __init__(self, db='online'):
         # initialize the database connection
         params = ""
@@ -97,7 +170,7 @@ class SCDButil:
 
         return title
 
-    def get_graph(self, channel, index, checkGood=True, time_interval='all'):
+    def get_graph(self, channel, index, checkGood=True, time_interval='all', scale_overflow=True):
         cur = self.execute_query(self.generate_sql_channel(channel=channel, checkGood=checkGood, time_interval=time_interval))
 
         gr = ROOT.TGraph()
@@ -107,7 +180,12 @@ class SCDButil:
             if not entry:
                 break
 
-            gr.SetPoint(i, time.mktime(entry[3].timetuple()), entry[2][index])
+            value = entry[2][index]
+            if value < -10.:
+                value = 25.
+            elif value > 50.:
+                value = 25.
+            gr.SetPoint(i, time.mktime(entry[3].timetuple()), value)
             i += 1
 
         return gr
@@ -136,10 +214,10 @@ class SCDButil:
         return gr
         
         
-    def plot_channel(self, channel, index, checkGood=True, time_interval='all'):
+    def plot_channel(self, channel, index, checkGood=True, time_interval='all', scale_overflow=True):
         #canvas = ROOT.TCanvas()
         #canvas.cd()
-        gr = self.get_graph(channel=channel, index=index, checkGood=checkGood, time_interval=time_interval)
+        gr = self.get_graph(channel=channel, index=index, checkGood=checkGood, time_interval=time_interval, scale_overflow=scale_overflow)
 
         gr.SetMarkerColor(ROOT.kBlue)
         gr.Draw('ap')
@@ -160,19 +238,29 @@ class SCDButil:
 
         return gr
 
-    def plot_channels(self, subchannel_list, canvas, checkGood=True, time_interval='all'):
+    def plot_channels(self, subchannel_list, checkGood=True, time_interval='all', title='', scale_overflow=True, fixed_scale=False, draw_legend=True):
         graphs = []
         mg = ROOT.TMultiGraph()
+        mg.SetTitle(title)
+        leg = ROOT.TLegend(0.85, 0.65, 0.95, 0.95)
         for entry in subchannel_list:
-            graphs.append(self.get_graph(channel=entry[0], index=entry[1], checkGood=checkGood, time_interval=time_interval))
+            gr = self.get_graph(channel=entry[0], index=entry[1], checkGood=checkGood, time_interval=time_interval, scale_overflow=scale_overflow)
+            gr.SetName(self.get_label(channel=entry[0], index=entry[1]))
+            if (entry[0], entry[1]) in self.color_dict.keys():
+                gr.SetMarkerColor(self.color_dict[ (entry[0], entry[1]) ])
+            else:
+                gr.SetMarkerColor(ROOT.kBlack)
+            graphs.append(gr)
 
-        canvas = ROOT.TCanvas()
+        #canvas = ROOT.TCanvas()
         first = True
         i = 0
         for gr in graphs:
             mg.Add(gr)
-            if first:
-                gr.SetMarkerColor(ROOT.kBlue)
+
+            
+            #if first:
+                #gr.SetMarkerColor(ROOT.kBlue)
                 #gr.Draw('ap')
                 #gr.GetXaxis().SetTimeDisplay(1)
                 #gr.GetXaxis().SetTimeFormat('#splitline{%b-%d}{%H:%M}')
@@ -187,11 +275,14 @@ class SCDButil:
                 #gr.GetYaxis().SetTitle(yaxis_title)
                 #gr.GetYaxis().SetTitleOffset(1.3)
 
-                first = False
-            else:
-                i += 1
-                gr.SetMarkerColor(ROOT.kBlue+i)
-                gr.Draw('a same')
+                #first = False
+            #else:
+                #i += 1
+                #gr.SetMarkerColor(ROOT.kBlue+i)
+                #gr.Draw('a same')
+
+            #print 'adding graph to legend..'
+            leg.AddEntry(gr, 'name', 'l')
 
         mg.Draw('ap')
         mg.GetXaxis().SetTimeDisplay(1)
@@ -199,7 +290,9 @@ class SCDButil:
         mg.GetXaxis().SetLabelSize(0.025)
         mg.GetXaxis().SetLabelOffset(0.02)
 
-        mg.SetTitle()
+        if draw_legend:
+            #print 'drawing legend...'
+            leg.Draw()
 
         yaxis_title = 'value'
         if subchannel_list[0][0].find('Temp') != -1:
@@ -208,6 +301,9 @@ class SCDButil:
             yaxis_title = 'ADC value (Volts)'
         mg.GetYaxis().SetTitle(yaxis_title)
         mg.GetYaxis().SetTitleOffset(1.3)
+
+        if fixed_scale:
+            mg.GetYaxis().SetRangeUser(25., 35.)
 
         return mg
 
@@ -218,7 +314,7 @@ if __name__ == '__main__':
     subchannel_list = [ ('mscb323_Temp_P1', 0), ('mscb323_Temp_P1', 1) ]
 
     canvas = ROOT.TCanvas('c1', 'c1', 1)
-    g = db.plot_channels(subchannel_list, canvas)
+    g = db.plot_channels(db.subchannel_dict['hall'])
     g.Draw('ap')
 
     #ROOT.gApplication.Run()
--- make_all_temp_plots.py
+++ make_all_temp_plots.py
@@ -14,11 +14,28 @@ import ROOT
 db = SCDButil.SCDButil()
 
 # create list of plots to make
-plot_list = ['hall']
+plot_list = ['hall', 'laser_hut', 'magnetA', 'magnetB', 'magnetC', 'magnetD', 'magnetE', 'magnetF', 'magnetG', 'magnetH', 'magnetI']
+#plot_list = ['magnetA', 'magnetB']
 
 # which time interval plots to make
 interval_list = ['all', 'week', 'day']
 
+leg_dict = {}
+
 for plot in plot_list:
-    g = db.plot_channels(db.subchannel_dict[plot], ROOT.TCanvas())
-    g.Draw('ap')
+    for interval in interval_list:
+        g = db.plot_channels(db.subchannel_dict[plot], time_interval=interval, title=plot + ', ' + interval, draw_legend=False, fixed_scale=False)
+    
+        canvas = ROOT.TCanvas(plot + '_' + interval + '_canvas', plot + '_' + interval + '_canvas', 1)
+        g.Draw('ap')
+
+        #leg_dict[ (plot, interval) ] = ROOT.TLegend(0.85, 0.65, 0.95, 0.95)
+        #print len(g.GetListOfGraphs())
+        #for i in range(len(g.GetListOfGraphs())):
+            #leg_dict[ (plot, interval) ].AddEntry(gr, gr.GetName(), 'l')
+            #print i
+        #leg_dict[ (plot, interval) ].Draw()
+
+        canvas.SaveAs(plot + '_' + interval + '.pdf')
+
+    
